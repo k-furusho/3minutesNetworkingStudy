394960948e07f5117fe1a7bca255997fce20022a
k-furusho <k.furusho@bbtech.jp>
2024-01-04 00:54:36 +0900

    add;GETリクエストの対応

--- http/server.test.ts
+++ http/server.test.ts
@@ -1,5 +1,8 @@
 import { describe, it, expect, beforeAll, afterAll } from "vitest";
 import * as net from "net";
+import * as fs from "fs";
+import * as path from "path";
+import fetch from "node-fetch";
 import server from "./server";
 
 const PORT = process.env.PORT ?? 8080;
@@ -48,29 +51,34 @@ describe("TCPサーバーテスト", () => {
 });
 
 describe("HTTPサーバーテスト", () => {
-  beforeAll((done) => {
-    server.listen(PORT);
-  });
-
-  afterAll((done) => {
-    server.close((e) => {
-      if (e != null) {
-        console.error(e);
-      }
+  beforeAll(() => {
+    return new Promise((resolve, reject) => {
+      server.listen(PORT, (err) => {
+        if (err) reject(err);
+        else resolve();
+      });
     });
   });
 
-  it("/index.htmlをリクエストしたとき200 OKが返されるべき", (done) => {
-    const client = new net.Socket();
-
-    client.connect({ port: PORT }, () => {
-      client.write("GET /index.html HTTP/1.1\r\n\r\n");
+  afterAll(() => {
+    return new Promise((resolve, reject) => {
+      server.close((err) => {
+        if (err) reject(err);
+        else resolve();
+      });
     });
+  });
 
-    client.on("data", (data) => {
-      expect(data.toString()).toContain("HTTP/1.0 200 OK");
-      client.destroy(); // ソケットを閉じる
-    });
+  it("【GETリクエスト】/index.htmlをリクエストしたとき /public/index.html が返されるべき", async () => {
+    const response = await fetch(`http://localhost:${PORT}/index.html`);
+
+    expect(response.ok).toBeTruthy();
+    expect(await response.text()).toBe(
+      fs.readFileSync(path.join(__dirname, "public", "index.html")).toString()
+    );
+    expect(response.headers.get("Content-Type")).toContain(
+      "text/html; charset=utf-8"
+    );
   });
 
   it("存在しないファイルにリクエストしたとき404 Not Foundが返されるべき", (done) => {
--- http/server.ts
+++ http/server.ts
@@ -1,6 +1,7 @@
 // https://nodejs.org/dist/latest-v20.x/docs/api/net.html#class-netsocket
 import * as net from "net";
 import * as fs from "fs";
+import * as path from "path";
 
 const server = net.createServer();
 server.on("connection", (socket) => {
@@ -10,12 +11,11 @@ server.on("connection", (socket) => {
     // リクエストラインの取得（例：GET /index.html HTTP/1.1）
     const requestLine = data.toString().split("\n")[0];
     const requestParts = requestLine.split(" ");
-    const requestedFile = requestParts[1];
+    const requestedFile = requestParts[1]; // 例：/index.html
 
-    // ルートパスへのリクエストをindex.htmlにマッピング
-    const filePath =
-      requestedFile === "/" ? "./index.html" : `.${requestedFile}`;
-
+    // パブリックディレクトリからファイルを読み込む
+    const filePath = path.join(__dirname, "public", requestedFile);
+    console.log("filePath：", filePath);
     // ファイルの読み込みと送信
     fs.readFile(filePath, (err, content) => {
       if (err) {
@@ -23,13 +23,29 @@ server.on("connection", (socket) => {
         socket.write("HTTP/1.0 404 Not Found\n\n");
         socket.end();
       } else {
+        // Content-Typeの決定
+        const contentType = getContentType(filePath);
         // 成功した場合の応答
-        socket.write("HTTP/1.0 200 OK\n\n");
+        socket.write(`HTTP/1.0 200 OK\r\nContent-Type: ${contentType}\r\n\r\n`);
         socket.write(content);
         socket.end();
       }
     });
   });
 });
+
+function getContentType(filePath: string): string {
+  const ext = path.extname(filePath);
+  switch (ext) {
+    case ".html":
+      return "text/html; charset=utf-8";
+    case ".js":
+      return "text/javascript; charset=utf-8";
+    case ".css":
+      return "text/css; charset=utf-8";
+    default:
+      return "text/plain; charset=utf-8";
+  }
+}
 
 export default server;
