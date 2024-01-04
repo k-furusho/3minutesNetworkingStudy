d52eab9c8c3c1f34dd37608543f27615ae64a594
k-furusho <k.furusho@bbtech.jp>
2024-01-05 00:36:26 +0900

    add:POSTリクエスト

--- http/server.ts
+++ http/server.ts
@@ -3,56 +3,71 @@ import * as net from "net";
 import * as fs from "fs";
 import * as path from "path";
 
-// const redirectRules = JSON.parse(fs.readFileSync("./redirect.json", "utf8"));
+// リダイレクトルールの読み込み
 const redirectRules = JSON.parse(
   fs.readFileSync(path.join(__dirname, "redirect.json"), "utf8")
 );
+
 const server = net.createServer();
+
 server.on("connection", (socket) => {
   socket.on("data", (data) => {
-    console.log(data.toString());
+    const requestData = data.toString();
 
-    const requestString = data.toString();
-    const requestLine = requestString.split("\n")[0];
-    const requestParts = requestLine.split(" ");
-    const method = requestParts[0]; // リクエストメソッド（GET、HEADなど）
-    const requestedFile = requestParts[1];
+    // リクエストヘッダとボディを分割
+    if (requestData.includes("\r\n\r\n")) {
+      const [header, body] = requestData.split("\r\n\r\n");
 
-    // リダイレクトルールのチェック
-    const redirectRule = redirectRules.find(
-      (rule: { from: string }) => rule.from === requestedFile
-    );
-    if (redirectRule) {
-      const statusCode =
-        redirectRule.type === "TEMPORARY"
-          ? "302 Found"
-          : "301 Moved Permanently";
-      socket.write(
-        `HTTP/1.1 ${statusCode}\r\nLocation: ${redirectRule.to}\r\n\r\n`
+      // リクエストメソッドとパスの解析
+      const requestLine = header.split("\n")[0];
+      const [method, requestedPath] = requestLine.split(" ");
+
+      // リダイレクトルールのチェック
+      const redirectRule = redirectRules.find(
+        (rule: { from: string }) => rule.from === requestedPath
       );
-      socket.end();
-      return;
-    }
-
-    // .public/からファイルを読み込む
-    const filePath = path.join(__dirname, "public", requestedFile);
-    // ファイルの読み込みと送信
-    fs.readFile(filePath, (err, content) => {
-      if (err) {
-        // ファイルが見つからない場合のエラー応答
-        socket.write("HTTP/1.0 404 Not Found\n\n");
+      if (redirectRule) {
+        const statusCode =
+          redirectRule.type === "TEMPORARY"
+            ? "302 Found"
+            : "301 Moved Permanently";
+        socket.write(
+          `HTTP/1.1 ${statusCode}\r\nLocation: ${redirectRule.to}\r\n\r\n`
+        );
         socket.end();
-      } else {
-        // Content-Typeの決定
-        const contentType = getContentType(filePath);
-        // 成功した場合の応答
-        socket.write(`HTTP/1.0 200 OK\r\nContent-Type: ${contentType}\r\n\r\n`);
-        if (method === "GET") {
-          socket.write(content);
-        }
+        return;
+      }
+
+      if (method === "GET" || method === "HEAD") {
+        // GET または HEAD リクエストの処理
+        // .public/からファイルを読み込む
+        const filePath = path.join(__dirname, "public", requestedPath);
+        // ファイルの読み込みと送信
+        fs.readFile(filePath, (err, content) => {
+          if (err) {
+            // ファイルが見つからない場合のエラー応答
+            socket.write("HTTP/1.0 404 Not Found\n\n");
+            socket.end();
+          } else {
+            // Content-Typeの決定
+            const contentType = getContentType(filePath);
+            // 成功した場合の応答
+            socket.write(
+              `HTTP/1.0 200 OK\r\nContent-Type: ${contentType}\r\n\r\n`
+            );
+            if (method === "GET") {
+              socket.write(content);
+            }
+            socket.end();
+          }
+        });
+      } else if (method === "POST") {
+        // POSTリクエストの処理
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        socket.write(body); // エコーバック
         socket.end();
       }
-    });
+    }
   });
 });
 
